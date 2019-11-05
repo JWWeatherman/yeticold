12114994b5a5e0d39e0a2773921fc60b2ca00e5e
jwWeatherman.com <32912678+JWWeatherman@users.noreply.github.com>
2019-11-05 14:04:59 -0500

    Removing the ~ in front of the file to tarball.

--- flaskapp/rpkg-script.sh
+++ flaskapp/rpkg-script.sh
@@ -4,5 +4,5 @@ fakeroot -u dpkg-repack `dpkg --get-selections | grep install | cut -f1`
 echo "Done Packaging"
 cd
 sudo chmod 777 ~/dpkg-repack
-tar -czf ToDisconnected.tar.gz ~/dpkg-repack
+tar -czf ToDisconnected.tar.gz dpkg-repack
 sudo chmod 777 ~/ToDisconnected.tar.gz
