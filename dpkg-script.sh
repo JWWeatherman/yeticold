bfb1b4e675ca82bc640dee9f86b03b058c30192f
jwWeatherman.com <32912678+JWWeatherman@users.noreply.github.com>
2019-11-07 22:07:35 -0500

    Update dpkg-script.sh

--- dpkg-script.sh
+++ dpkg-script.sh
@@ -1,4 +1,5 @@
 tar -xzf ToDisconnected.tar.gz
 cd ~/dpkg-repack
 sudo dpkg -i *.deb
+pip3 install r ~/yeticold/reqs.txt --no-index --find-links ~/dpkg-repack/wheelhouse
 echo "Done unpackaging. Close this terminal window now."
