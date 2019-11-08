43b23bbfac27d7e328cab090146fb99e94ad0b16
jwWeatherman.com <32912678+JWWeatherman@users.noreply.github.com>
2019-11-07 19:25:07 -0500

    Added more packaging.

--- rpkg-script.sh
+++ rpkg-script.sh
@@ -3,6 +3,8 @@ mkdir ~/dpkg-repack; cd ~/dpkg-repack
 fakeroot -u dpkg-repack `dpkg --get-selections | grep install | cut -f1`
 cd
 sudo chmod 777 ~/dpkg-repack
+mkdir ~/dpkg-repack/wheelhouse
+pip3 download -r ~/yeticold/reqs.txt -d ~/dpkg-repack/wheelhouse
 tar -czf ToDisconnected.tar.gz dpkg-repack
 sudo chmod 777 ~/ToDisconnected.tar.gz
 echo "Done Packaging"
