8b8c6d11b283d26057a76aa5a442f68cdfd82bd5
Will Weatherman <will@mathbot.com>
2019-11-15 12:02:30 -0500

    Fixed qrcode verifier

--- yeticoldR.py
+++ yeticoldR.py
@@ -357,7 +357,7 @@ def step11():
         if (secondqrcode.split(':')[0] == 'bitcoin'):
             secondqrcode = secondqrcode.split(':')[1].split('?')[0]
         if (secondqrcode.split('1')[0] == 'bc') or (secondqrcode[:1] == '3') or (secondqrcode[:1] == '1'):
-            if (len(secondqrcode) >= 26) or (len(secondqrcode) <= 35):
+            if not (len(secondqrcode) >= 26) or (len(secondqrcode) <= 35):
                 error = secondqrcode + ' is not a valid bitcoin address'
         else: 
             error = secondqrcode + ' is not a valid bitcoin address'
