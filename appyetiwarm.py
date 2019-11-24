bc64238459537d2947bd9115250acdd0b06af2b3
willweatherman <will@mathbot.com>
2019-11-24 16:03:12 -0500

    error logging fix

--- appyetiwarm.py
+++ appyetiwarm.py
@@ -353,6 +353,8 @@ def step08():
         response = subprocess.Popen(['~/yeticold/bitcoin-0.19.0rc1/bin/bitcoin-cli -rpcwallet=yetiwarm getdescriptorinfo "wsh(multi(3,'+xprivlist[0]+'/*,'+xprivlist[1]+'/*,'+xprivlist[2]+'/*,'+xprivlist[3]+'/*,'+xprivlist[4]+'/*,'+xprivlist[5]+'/*,'+xprivlist[6]+'/*))"'],shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         print(response)
         response = json.loads(response[0].decode("utf-8"))
+        print(response)
+        print(response["descriptor"])
         checksum = response["checksum"]
         pubdesc = response["descriptor"]
         return redirect('/step0915')
