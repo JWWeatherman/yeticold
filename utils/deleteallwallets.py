00c496f3dbf5842316e0de6343bfc5e8602f47c4
willweatherman <will@mathbot.com>
2019-11-24 16:44:11 -0500

    fixed wallet deleteing

--- utils/deleteallwallets.py
+++ utils/deleteallwallets.py
@@ -4,4 +4,7 @@ import subprocess
 subprocess.call('sudo rm -r ~/.bitcoin/yeticold*', shell=True)
 subprocess.call('sudo rm -r ~/.bitcoin/yetiwarm*', shell=True)
 subprocess.call('sudo rm -r ~/.bitcoin/yetihot*', shell=True)
+subprocess.call('sudo rm -r ~/yeticoldwallet*', shell=True)
+subprocess.call('sudo rm -r ~/yetiwarmwallet*', shell=True)
+subprocess.call('sudo rm -r ~/yetihotwallet*', shell=True)
 subprocess.call('echo "DONE"', shell=True)
