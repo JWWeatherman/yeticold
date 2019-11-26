8ed3eca8f1faabed376568788b350710c0ebae63
willweatherman <will@mathbot.com>
2019-11-26 09:14:40 -0500

    fixed typo

--- utils/stopbitcoin.py
+++ utils/stopbitcoin.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 home = os.getenv("HOME")
 subprocess.call('~/yeticold/bitcoin-0.19.0rc1/bin/bitcoin-cli stop', shell=True)
-while os.path.exists(home + "/.bitcoin/bitcoin.pid"):
+while os.path.exists(home + "/.bitcoin/bitcoind.pid"):
 	i = "random stuff untill bitcoin stops"
