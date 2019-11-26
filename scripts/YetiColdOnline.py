a7bc27608c9b3104ddfad0e9355cab7f660174e9
willweatherman <will@mathbot.com>
2019-11-26 14:55:04 -0500

    fixed scripts

--- scripts/YetiColdOnline.py
+++ scripts/YetiColdOnline.py
@@ -2,14 +2,7 @@ import os
 import subprocess
 home = os.getenv("HOME")
 
-subprocess.call(['cd ~/yeticold; git pull; cd'],shell=True)
-subprocess.call(['sudo apt-get install python3-venv'],shell=True)
-subprocess.call(['sudo apt-get install python3-pip'],shell=True)
-subprocess.call(['sudo apt-get install libzbar0'],shell=True)
-subprocess.call(['sudo apt install tor'],shell=True)
-subprocess.call('fuser -k 5000/tcp', shell=True)
-subprocess.call(['sudo pip3 install python-bitcoinrpc'],shell=True)
-subprocess.call(['pip3 install opencv-python'],shell=True)
+
 if not (os.system("python3 -c 'import flask'") == 0):
 	subprocess.call(['pip3 install flask'],shell=True)
 if not (os.system("python3 -c 'import qrtools'") == 0):
@@ -25,6 +18,14 @@ if not (os.system("python3 -c 'import zbar'") == 0):
 	
 
 if not (os.path.exists(home + "/yeticold/bitcoin-0.19.0rc1/bin")):
+	subprocess.call(['cd ~/yeticold; git pull; cd'],shell=True)
+	subprocess.call(['sudo apt-get install python3-venv'],shell=True)
+	subprocess.call(['sudo apt-get install python3-pip'],shell=True)
+	subprocess.call(['sudo apt-get install libzbar0'],shell=True)
+	subprocess.call(['sudo apt install tor'],shell=True)
+	subprocess.call('fuser -k 5000/tcp', shell=True)
+	subprocess.call(['sudo pip3 install python-bitcoinrpc'],shell=True)
+	subprocess.call(['pip3 install opencv-python'],shell=True)
 	subprocess.call(['sudo apt-get update'],shell=True)
 	subprocess.call(['echo "Installing updates. This could take an hour without feedback."'],shell=True)
 	subprocess.call(['sudo unattended-upgrade'],shell=True)
