2d926a7267cd01c3d414ecf2ccec096f5301452c
willweatherman <will@mathbot.com>
2019-11-27 09:32:22 -0500

    test

--- utils/stopbitcoin.py
+++ utils/stopbitcoin.py
@@ -3,7 +3,7 @@ import subprocess
 home = os.getenv("HOME")
 subprocess.call('~/yeticold/bitcoin-0.19.0rc1/bin/bitcoin-cli stop', shell=True)
 i = 0
-while os.path.exists(home + "/.bitcoin/bitcoind.pid") and not (subprocess.call('lsof -n -i :8332', shell=True) == 1):
+while os.path.exists(home + "/.bitcoin/bitcoind.pid") and (subprocess.call('lsof -n -i :8332', shell=True) != 1):
 	print("ih")
 	i = i + 1
 	if (subprocess.call('lsof -n -i :8332', shell=True) == 1) and (i > 2000):
