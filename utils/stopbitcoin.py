f65f699af01f160110794bd9aefbca60e4323c20
willweatherman <will@mathbot.com>
2019-11-27 09:26:45 -0500

    fixed bug with closing bitcoin

--- utils/stopbitcoin.py
+++ utils/stopbitcoin.py
@@ -3,4 +3,6 @@ import subprocess
 home = os.getenv("HOME")
 subprocess.call('~/yeticold/bitcoin-0.19.0rc1/bin/bitcoin-cli stop', shell=True)
 while os.path.exists(home + "/.bitcoin/bitcoind.pid"):
+	if (subprocess.call('lsof -n -i :8332', shell=True) == 1):
+		subprocess.call('rm -r ~/.bitcoin/bitcoin.pid', shell=True)
 	i = "random stuff untill bitcoin stops"
