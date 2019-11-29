fa93b202e9fec85aa46031b6b8be617ab73dec45
willweatherman <will@mathbot.com>
2019-11-29 15:08:55 -0500

    fixed qr bug

--- appyetiwarm.py
+++ appyetiwarm.py
@@ -378,9 +378,11 @@ def YWgetseeds():
         print(response)
         response = json.loads(response[0].decode("utf-8"))
         print(response)
-        print(response["descriptor"])
         checksum = response["checksum"]
         pubdesc = response["descriptor"].replace('\n', '')
+        print()
+        print(pubdesc)
+        print()
         response = subprocess.Popen(['~/yeticold/bitcoin-0.19.0rc1/bin/bitcoin-cli -rpcwallet=yetiwarm importmulti "'+pubdesc+'"'],shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
         print(response)
         return redirect('/YWdisplayseeds')
