1450cecd097ece6bcd2ab00a5e97c58572f37f16
willweatherman <will@mathbot.com>
2019-12-02 13:54:19 -0500

    changed bitcoin folder

--- utils/downloadbitcoin.py
+++ utils/downloadbitcoin.py
@@ -1,4 +1,4 @@
 import subprocess
-subprocess.call(['wget https://bitcoincore.org/bin/bitcoin-core-0.19.0/test.rc1/bitcoin-0.19.0rc1-x86_64-linux-gnu.tar.gz -P ~/yeticold/'],shell=True)
-subprocess.call(['tar xvzf ~/yeticold/bitcoin-0.19.0rc1-x86_64-linux-gnu.tar.gz -C ~/yeticold'],shell=True)
-subprocess.call(['mv ~/yeticold/bitcoin-0.19.0rc1 ~/yeticold/bitcoin'],shell=True)
+subprocess.call(['wget https://bitcoincore.org/bin/bitcoin-core-0.19.0.1/bitcoin-0.19.0.1-x86_64-linux-gnu.tar.gz -P ~/yeticold/'],shell=True)
+subprocess.call(['tar xvzf ~/yeticold/bitcoin-0.19.0.1-x86_64-linux-gnu.tar.gz -C ~/yeticold'],shell=True)
+subprocess.call(['mv ~/yeticold/bitcoin-0.19.0.1 ~/yeticold/bitcoin'],shell=True)
