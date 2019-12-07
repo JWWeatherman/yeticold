3badf470610f55ae328fc15d099ec11ff89b52e1
willweatherman <will@mathbot.com>
2019-12-07 09:17:17 -0500

    fixed bitcoin bug

--- utils/stopbitcoin.py
+++ utils/stopbitcoin.py
@@ -1,7 +1,9 @@
 import os
 import subprocess
+import time
 home = os.getenv("HOME")
 subprocess.call('~/yeticold/bitcoin/bin/bitcoin-cli stop', shell=True)
 while os.path.exists(home + "/.bitcoin/bitcoind.pid") or (subprocess.call('lsof -n -i :8332', shell=True) != 1):
 	if os.path.exists(home + "/.bitcoin/bitcoind.pid") and (subprocess.call('lsof -n -i :8332', shell=True) == 1):
 		subprocess.call('rm -r ~/.bitcoin/bitcoind.pid', shell=True)
+time.sleep(1)
