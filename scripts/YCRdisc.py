7ff450971267baf5617f91b4529cb4144e0243c9
willweatherman <will@mathbot.com>
2019-12-19 16:48:43 -0500

    packageing

--- scripts/YCRdisc.py
+++ scripts/YCRdisc.py
@@ -3,7 +3,7 @@ import os
 home = os.getenv("HOME")
 
 if not (os.path.exists(home + "/yeticold")) or not (os.path.exists(home + "/.bitcoin")):
-	subprocess.call(['cat ~ToDisconnected/ToDisconnected.tar.gz.part* >ToDisconnected.tar.gz'],shell=True)
+	subprocess.call(['cat ~/ToDisconnected/ToDisconnected.tar.gz.part* >ToDisconnected.tar.gz'],shell=True)
 	subprocess.call(['tar -xzf ToDisconnected.tar.gz'],shell=True)
 if not (os.system("python3 -c 'import flask'") == 0):
 	subprocess.call(['bash -c "sudo chmod +x ~/yeticold/scripts/dpkg-script.sh; sudo ~/yeticold/scripts/dpkg-script.sh"'],shell=True)
