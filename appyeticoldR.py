7933645c7c8fb387ac6ab90d85299479fc98aa5d
willweatherman <will@mathbot.com>
2020-01-02 10:15:03 -0500

    bug fix

--- appyeticoldR.py
+++ appyeticoldR.py
@@ -249,7 +249,7 @@ def YCRblockchain():
             return redirect('/YCRopenbitcoin')
     if request.method == 'POST':
         if request.form['option'] == 'downloadblockchain':
-            subprocess.call(['wsh https://drive.google.com/uc?authuser=0&id=1qjsuk1mllQMcWKmWZXhDQ9eRL7hL7aLA&export=download'],shell=True)
+            subprocess.call(['wget https://drive.google.com/uc?authuser=0&id=1qjsuk1mllQMcWKmWZXhDQ9eRL7hL7aLA&export=download'],shell=True)
             subprocess.call(['tar -xzf .bitcoin.tar.gz'],shell=True)
         else:
             fmt = '%Y-%m-%d %H:%M:%S'
