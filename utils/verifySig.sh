af3f9698a54b7e44d421cec715e78b264e48893d
willweatherman <will@mathbot.com>
2020-01-16 15:12:05 -0500

    added signature checking

--- utils/verifySig.sh
+++ utils/verifySig.sh
@@ -1,13 +1,3 @@
-if [ -f "~/yeticold/sigcorrect" ]
-then
-  rm ~/yeticold/sigcorrect
-fi
-
-if [ ! -f "~/yeticold/SHA256SUMS.asc" ] 
-then
-  sudo wget https://bitcoin.org/bin/bitcoin-core-0.19.0.1/SHA256SUMS.asc -P ~/yeticold/
-fi
-
 Sig=$(sha256sum --ignore-missing --check SHA256SUMS.asc)
 CheckSig=$(echo ${Sig} | grep 'bitcoin-0.19.0.1-x86_64-linux-gnu.tar.gz: OK' -c)
 echo ${CheckSig}
