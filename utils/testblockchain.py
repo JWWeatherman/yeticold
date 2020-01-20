d9f2f37507e922d1cb93d737768351740f084799
willweatherman <will@mathbot.com>
2020-01-20 15:25:23 -0500

    added gdown.pl code

--- utils/testblockchain.py
+++ utils/testblockchain.py
@@ -2,8 +2,7 @@ import os
 import subprocess
 home = os.getenv("HOME")
 if not (os.path.exists(home + "/.bitcoin")):
-	if not (os.path.exists(home + "yeticold/utils/.bitcoin")):
-		subprocess.call(['cd ~/yeticold/utils; perl gdown.pl https://drive.google.com/file/d/1iTLKVLkvAaGRDE7dnLpSR5Hl4I-Xnyft/view?usp=sharing .bitcoin.tar.gz; cd'],shell=True)
-		subprocess.call(['tar -xzvf .bitcoin.tar.gz .bitcoin'],shell=True)
+	if not (os.path.exists(home + "yeticold/utils/.bitcoin.tar.gz")):
+		subprocess.call(['cd ~/yeticold/utils; perl gdown.pl https://drive.google.com/file/d/1iTLKVLkvAaGRDE7dnLpSR5Hl4I-Xnyft/view?usp=sharing .bitcoin.tar.gz; rm -r .bitcoin; tar -xzvf .bitcoin.tar.gz .bitcoin; cd'],shell=True)
 	subprocess.call(['mv ~/yeticold/utils/.bitcoin ~/'],shell=True)
 
