414dd86d8806c58a9f25cc351061ac37d10f57ed
Jeremiah Valenzuela <github@julianoptics.com>
2020-08-05 21:37:24 -0400

    Clean up code & add docstring
    'rm ~/testblockchain' wasn't needed as that folder is never created.
    
    Use os.path.join for contructing folder and file paths in an agnostic
    way.
    
    Convert the subprocess.call functions into
    subprocess.run functions
    
    Replace ; in bash command with && so it only runs if the previous was
    successful
    
    Change `cd` to `cd ~` for clarity and explicitness

--- utils/testblockchain.py
+++ utils/testblockchain.py
@@ -1,8 +1,13 @@
+"""
+This utility function allows for the download of a recent chunk of the blockchain.
+It's useful for testing and continuing through the YetiCold process, but not for 
+actual wallet use. ~/.bitcoin folder should be deleted before using YetiCold for
+real.
+"""
+
 import os
 import subprocess
-home = os.getenv("HOME")
-if not (os.path.exists(home + "/.bitcoin")):
-	subprocess.call(['cd ~/yeticold/utils; rm ~/yeticold/utils/.bitcoin.tar.gz; perl gdown.pl https://drive.google.com/file/d/1iTLKVLkvAaGRDE7dnLpSR5Hl4I-Xnyft/view?usp=sharing .bitcoin.tar.gz; rm -r ~/yeticold/utils/.bitcoin; tar -xzvf .bitcoin.tar.gz .bitcoin; cd'],shell=True)
-	subprocess.call(['mv ~/yeticold/utils/.bitcoin ~/'],shell=True)
-	subprocess.call(['rm ~/testblockchain'],shell=True)
-
+HOME = os.getenv("HOME")
+if not os.path.exists(os.path.join(HOME, ".bitcoin")):
+    subprocess.run('cd ~/yeticold/utils && rm ~/yeticold/utils/.bitcoin.tar.gz && perl gdown.pl https://drive.google.com/file/d/1iTLKVLkvAaGRDE7dnLpSR5Hl4I-Xnyft/view?usp=sharing .bitcoin.tar.gz && rm -r ~/yeticold/utils/.bitcoin && tar -xzvf .bitcoin.tar.gz .bitcoin && cd ~', shell=True, check=False)
+    subprocess.run('mv ~/yeticold/utils/.bitcoin ~/', shell=True, check=False)
