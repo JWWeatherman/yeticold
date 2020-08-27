fd1f40d90909baf00d04603bd65dbd0c6750efda
willweatherman <will@mathbot.com>
2020-08-27 12:08:10 -0600

    refactor yeti

--- appyetiwarm.py
+++ appyetiwarm.py
@@ -7,6 +7,7 @@ import variables as v
 v.rpcpsw = str(random.randrange(0,1000000))
 v.settings = {"rpc_username": "rpcuser","rpc_password": v.rpcpsw, "rpc_host": "127.0.0.1","rpc_port": 8332,"address_chunk": 100}
 v.wallet_template = "http://{rpc_username}:{rpc_password}@{rpc_host}:{rpc_port}/wallet/{wallet_name}"
+print(v.rpcpsw)
 from formating import *
 from btcrpcfunctions import *
 from yetifunctions import *
@@ -20,7 +21,7 @@ rpc = RPC("yetiwarm")
 def handle_bad_request(e):
     if e.original_exception != None:
         e = e.original_exception
-    return render_template('v.error.html', error=e), 500
+    return render_template('error.html', error=e), 500
 
 # @app.route("/test", methods=['GET', 'POST'])       
 # def test():
