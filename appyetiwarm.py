ecb5e6778f7d15e029af12dd43725a00f57d5dce
willweatherman <will@mathbot.com>
2020-08-27 11:50:38 -0600

    refactor yeti

--- appyetiwarm.py
+++ appyetiwarm.py
@@ -3,10 +3,10 @@ import sys
 home = os.getenv("HOME")
 sys.path.append(home + '/yeticold/utils/')
 from imports import *
-from variables import *
-rpcpsw = str(random.randrange(0,1000000))
-settings = {"rpc_username": "rpcuser","rpc_password": rpcpsw,"rpc_host": "127.0.0.1","rpc_port": 8332,"address_chunk": 100}
-wallet_template = "http://{rpc_username}:{rpc_password}@{rpc_host}:{rpc_port}/wallet/{wallet_name}"
+import variables as v
+v.rpcpsw = str(random.randrange(0,1000000))
+v.settings = {"rpc_username": "rpcuser","rpc_password": rpcpsw,"rpc_host": "127.0.0.1","rpc_port": 8332,"address_chunk": 100}
+v.wallet_template = "http://{rpc_username}:{rpc_password}@{rpc_host}:{rpc_port}/wallet/{wallet_name}"
 from formating import *
 from btcrpcfunctions import *
 from yetifunctions import *
@@ -16,11 +16,11 @@ app = Flask(__name__)
 rpc = RPC("yetiwarm")
 
 
-@app.errorhandler(werkzeug.exceptions.InternalServerError)
+@app.v.errorhandler(werkzeug.exceptions.InternalServerError)
 def handle_bad_request(e):
     if e.original_exception != None:
         e = e.original_exception
-    return render_template('error.html', error=e), 500
+    return render_template('v.error.html', error=e), 500
 
 # @app.route("/test", methods=['GET', 'POST'])       
 # def test():
@@ -30,7 +30,7 @@ def handle_bad_request(e):
 #         print(thirdqrcode)
 #         print("hi")
 #         print(thirdqrcode)
-#     return render_template('error.html', error="hi")
+#     return render_template('v.error.html', error="hi")
 
 
 #ROUTES
@@ -49,12 +49,10 @@ def YWblockchain():
 
 @app.route("/YWopenbitcoin", methods=['GET', 'POST'])
 def YWopenbitcoin():
-    global progress
-    global IBD
     route = openBitcoin(request, '/YWopenbitcoin', '/YWmenu')
     if route:
         return route
-    return render_template('YWopenbitcoin.html', progress=progress, IBD=IBD)
+    return render_template('YWopenbitcoin.html', progress=v.progress, IBD=v.IBD)
 
 
 @app.route("/YWmenu", methods=['GET', 'POST'])
@@ -74,29 +72,23 @@ def YWgetseeds():
 #display for print
 @app.route("/YWprintdescriptor", methods=['GET', 'POST'])
 def YWprintdescriptor():
-    global pubdesc
     if request.method == 'GET':
-        path = makeQrCode(pubdesc)
+        path = makeQrCode(v.pubdesc)
     if request.method == 'POST':
         return redirect('/YWdisplayseeds')
-    return render_template('YWprintdescriptor.html', qrdata=pubdesc, path=path)
+    return render_template('YWprintdescriptor.html', qrdata=v.pubdesc, path=v.path)
 
 @app.route('/YWdisplayseeds', methods=['GET', 'POST'])
 def YWdisplayseeds():
-    global privkeylist
-    global privkeycount
     route = displaySeeds(request, '/YWdisplayseeds', '/YWcheckseeds')
     if route != None:
         return route
-    return render_template('YWdisplayseeds.html', PPL=passphraselist, x=privkeycount + 1, i=privkeycount + 9)
+    return render_template('YWdisplayseeds.html', PPL=v.passphraselist, x=v.privkeycount + 1, i=v.privkeycount + 9)
 
 @app.route('/YWcheckseeds', methods=['GET', 'POST'])
 def YWcheckseeds():
-    global privkeycount
-    global error
-    global oldkeys
     checkSeeds(request, '/YWcheckseeds', '/YWcopyseeds')
-    return render_template('YWcheckseeds.html', x=privkeycount + 1, error=error,i=privkeycount + 16,oldkeys=oldkeys)
+    return render_template('YWcheckseeds.html', x=v.privkeycount + 1, error=v.error,i=v.privkeycount + 16,oldkeys=v.oldkeys)
 
 @app.route("/YWcopyseeds", methods=['GET', 'POST'])
 def YWcopyseeds():
@@ -106,17 +98,15 @@ def YWcopyseeds():
 
 @app.route("/YWRscandescriptor", methods=['GET', 'POST'])
 def YWRscandescriptor():
-    global pubdesc
     if request.method == 'POST':
         pubdesc = handleResponse('python3 ~/yeticold/utils/scanqrcode.py').replace('\n', '')
         return redirect('/YWRrescanwallet')
-    return render_template('YWRscandescriptor.html', pubdesc=pubdesc)
+    return render_template('YWRscandescriptor.html', pubdesc=v.pubdesc)
 
 @app.route("/YWRrescanwallet", methods=['GET', 'POST'])
 def YWRrescanwallet():
-    global pubdesc
     if request.method == 'GET':
-        handleResponse('~/yeticold/bitcoin/bin/bitcoin-cli -rpcwallet=yetiwallet importmulti \'[{ "desc": "'+pubdesc+'", "timestamp": "now", "range": [0,999], "watchonly": false}]\' \'{"rescan": true}\'')
+        handleResponse('~/yeticold/bitcoin/bin/bitcoin-cli -rpcwallet=yetiwallet importmulti \'[{ "desc": "'+v.pubdesc+'", "timestamp": "now", "range": [0,999], "watchonly": false}]\' \'{"rescan": true}\'')
         handleResponse('~/yeticold/bitcoin/bin/bitcoin-cli -rpcwallet=yetiwallet rescanblockchain '+blockheight())
     if request.method == 'POST':
         return redirect('/YWRdisplaywallet')
@@ -124,12 +114,9 @@ def YWRrescanwallet():
 
 @app.route("/YWRdisplaywallet", methods=['GET', 'POST'])
 def YWRdisplaywallet():
-    global addresses
-    global color
-    global sourceaddress
     if request.method == 'GET':
-        addresses = []
-        totalwalletbal = 0
+        v.addresses = []
+        v.totalwalletbal = 0
         subprocess.call(['rm -r ~/yeticold/static/qrcode*'],shell=True)
         adrlist = handleResponse('~/yeticold/bitcoin/bin/bitcoin-cli -rpcwallet=yetiwallet deriveaddresses "'+pubdesc+'" "[0,999]"', True)
         rpc = RPC("yetiwallet")
@@ -152,7 +139,7 @@ def YWRdisplaywallet():
                 address['numbal'] = 0
                 address['status'] = status
                 address['route'] = route
-                addresses.append(address)
+                v.addresses.append(address)
             else:
                 for x in range(0, len(response)):
                     utxo = response[x]
@@ -160,7 +147,7 @@ def YWRdisplaywallet():
                     vout = utxo['vout']
                     scriptPubKey = utxo['scriptPubKey']
                     numamount = utxo['amount']
-                    totalwalletbal = totalwalletbal + numamount
+                    v.totalwalletbal = v.totalwalletbal + numamount
                     amount = "{:.8f}".format(float(numamount))
                     confs = utxo['confirmations']
                     totalbal = rpc.getreceivedbyaddress(adr)
@@ -182,72 +169,59 @@ def YWRdisplaywallet():
                     address['numbal'] = numamount
                     address['status'] = status
                     address['route'] = route
-                    addresses.append(address)
-        addresses.sort(key=lambda x: x['balance'], reverse=True)
-        for i in range(0, len(addresses)):
-            makeQrCode(addresses[i]['address'], home+'/yeticold/'+addresses[i]['route'])
+                    v.addresses.append(address)
+        v.addresses.sort(key=lambda x: x['balance'], reverse=True)
+        for i in range(0, len(v.addresses)):
+            makeQrCode(v.addresses[i]['address'], home+'/yeticold/'+v.addresses[i]['route'])
     if request.method == 'POST':
-        for i in range(0, len(addresses)):
-            if addresses[i]['txid'] == request.form['txid']:
-                sourceaddress = addresses[i]
+        for i in range(0, len(v.addresses)):
+            if v.addresses[i]['txid'] == request.form['txid']:
+                sourceaddress = v.addresses[i]
         return redirect('/YWRscanrecipent')
-    return render_template('YWRdisplaywallet.html', addresses=addresses, len=len(addresses), TWB=totalwalletbal)
+    return render_template('YWRdisplaywallet.html', addresses=v.addresses, len=len(v.addresses), TWB=v.totalwalletbal)
 
 @app.route("/YWRscanrecipent", methods=['GET', 'POST'])
 def YWRscanrecipent():
-    global error
-    global receipentaddress
     if request.method == 'POST':
-        error = None
+        v.error = None
         if request.form['option'] == 'scan':
-            receipentaddress = handleResponse('python3 ~/yeticold/utils/scanqrcode.py').replace('\n', '')
+            v.receipentaddress = handleResponse('python3 ~/yeticold/utils/scanqrcode.py').replace('\n', '')
         else:
-            receipentaddress = request.form['option']
-        if (receipentaddress.split(':')[0] == 'bitcoin'):
-            receipentaddress = receipentaddress.split(':')[1].split('?')[0]
-        if (receipentaddress[:3] == 'bc1') or (receipentaddress[:1] == '3') or (receipentaddress[:1] == '1'):
-            if not (len(receipentaddress) >= 26) and (len(receipentaddress) <= 35):
-                error = receipentaddress + ' is not a valid bitcoin address, address should have a length from 26 to 35 instead of ' + str(len(receipentaddress)) + '.'
+            v.receipentaddress = request.form['option']
+        if (v.receipentaddress.split(':')[0] == 'bitcoin'):
+            v.receipentaddress = v.receipentaddress.split(':')[1].split('?')[0]
+        if (v.receipentaddress[:3] == 'bc1') or (v.receipentaddress[:1] == '3') or (v.receipentaddress[:1] == '1'):
+            if not (len(v.receipentaddress) >= 26) and (len(v.receipentaddress) <= 35):
+                v.error = v.receipentaddress + ' is not a valid bitcoin address, address should have a length from 26 to 35 instead of ' + str(len(v.receipentaddress)) + '.'
         else: 
-            error = receipentaddress + ' is not a valid bitcoin address, address should have started with bc1, 3 or 1 instead of ' + receipentaddress[:1] + ', or ' + receipentaddress[:3] + '.'
-        if error:
+            v.error = v.receipentaddress + ' is not a valid bitcoin address, address should have started with bc1, 3 or 1 instead of ' + v.receipentaddress[:1] + ', or ' + v.receipentaddress[:3] + '.'
+        if v.error:
             return redirect('/YWRscanrecipent')
         return redirect('/YWRimportseeds')
-    return render_template('YWRscanrecipent.html', error=error, recipent=receipentaddress)
+    return render_template('YWRscanrecipent.html', error=v.error, recipent=v.receipentaddress)
 
 @app.route('/YWRimportseeds', methods=['GET', 'POST'])
 def YWRimportseeds():
-    global error
-    global privkeycount
     importSeeds(request, '/YWRimportseeds', '/YWRsendtransaction')
-    return render_template('YWRimportseeds.html', x=privkeycount + 1, error=error,i=privkeycount + 2 )
+    return render_template('YWRimportseeds.html', x=v.privkeycount + 1, error=v.error,i=v.privkeycount + 2 )
 
 #GEN trans qr code
 @app.route("/YWRsendtransaction", methods=['GET', 'POST'])
 def YWRsendtransaction():
-    global error
-    global receipentaddress
-    global minerfee
-    global amo
+
     sendTransaction(request, '/YWRsendtransaction', '/YWRdisplaywallet')
-    return render_template('YWRsendtransaction.html', amount=amo, minerfee=minerfee, recipent=receipentaddress, error=error)
+    return render_template('YWRsendtransaction.html', amount=v.amo, minerfee=v.minerfee, recipent=v.receipentaddress, error=v.error)
 
 #GEN trans qr code
 @app.route("/YWRsendtransactionB", methods=['GET', 'POST'])
-def YWRsendtransactionB():
-    global error
-    global receipentaddress
-    global minerfee
-    global amo
     sendTransaction(request, '/YWRsendtransactionB', '/YWRdisplaywallet')
-    return render_template('YWRsendtransactionB.html', amount=amo, minerfee=minerfee, recipent=receipentaddress, error=error)
+    return render_template('YWRsendtransactionB.html', amount=v.amo, minerfee=v.minerfee, recipent=v.receipentaddress, error=v.error)
 
 @app.route("/YWopenbitcoinB", methods=['GET', 'POST'])
 def YWopenbitcoinB():
-    global progress
-    global IBD
+
     openBitcoin(request, '/YWopenbitcoinB', '/YWRimportseeds')
-    return render_template('YWopenbitcoinB.html', progress=progress, IBD=IBD)
+    return render_template('YWopenbitcoinB.html', progress=v.progress, IBD=v.IBD)
 
 if __name__ == "__main__":
     app.run()
