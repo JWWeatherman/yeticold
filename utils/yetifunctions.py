83c1ac34e54eca733445de2417a5297dd7d4a5dd
willweatherman <will@mathbot.com>
2020-08-30 10:57:57 -0600

    refactor yeti

--- utils/yetifunctions.py
+++ utils/yetifunctions.py
@@ -32,7 +32,7 @@ def generatePrivKeys(genbinary=False):
     privkeylisttemp = []
     for i in range(1,8):
     	if genbinary:
-    	    newbinary = str('1') * 256
+            newbinary = str('1') * 256
         else:
             newbinary = request.form['binary' + str(i)]
         rpc = RPC()
