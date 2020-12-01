50d7d35d9af56642cf15ca2b08e56471e5a326a5
willweatherman <will@mathbot.com>
2020-11-30 20:50:40 -0500

    refactor yeti

--- utils/stopbitcoin.py
+++ utils/stopbitcoin.py
@@ -4,6 +4,6 @@ import time
 home = os.getenv("HOME")
 subprocess.call('bitcoin-cli stop', shell=True)
 while os.path.exists(home + "/.bitcoin/bitcoind.pid") or (subprocess.call('lsof -n -i :8332', shell=True) != 1):
-	if os.path.exists(home + "/.bitcoin/bitcoind.pid") and (subprocess.call('lsof -n -i :8332', shell=True) == 1):
-		subprocess.call('rm -r ~/.bitcoin/bitcoind.pid', shell=True)
+	# if os.path.exists(home + "/.bitcoin/bitcoind.pid") and (subprocess.call('lsof -n -i :8332', shell=True) == 1):
+	# 	subprocess.call('rm -r ~/.bitcoin/bitcoind.pid', shell=True)
 time.sleep(1)
