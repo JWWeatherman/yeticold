8d149f1f4991d59b7b0d762dead7988c50c14b42
willweatherman <will@mathbot.com>
2020-12-03 16:00:54 -0500

    refactor yeti

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -25,10 +25,10 @@ else:
 
     # Check if Bitcoin Core has been installed
     if not os.path.exists(HOME + "/yeticold/bitcoin"):
-        print("Installing updates. This could take an hour without feedback.")
         # Pipe 'yes' command to 'sudo apt install' to automate acceptance of installation
         # Use apt instead of apt-get since apt is more suitable for end users and has a graphical progress bar
         subprocess.run('yes | sudo apt install python3-pip sshpass=1.06-1 libzbar0=0.23-1.3 tor=0.4.2.7-1', shell=True, check=False)
+        print("Installing updates. This could take an hour without feedback.")
         subprocess.run('sudo unattended-upgrade', shell=True, check=False)
         subprocess.run('pip3 install --upgrade pip', shell=True, check=False)
     subprocess.run('python3 ~/yeticold/utils/downloadbitcoin.py', shell=True, check=False)
