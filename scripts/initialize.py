c423a0b6877161d17d00fad85e0f3a8a06b1c1c9
willweatherman <will@mathbot.com>
2020-12-04 15:10:56 -0500

    refactor yeti

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -29,34 +29,32 @@ else:
         # Use apt instead of apt-get since apt is more suitable for end users and has a graphical progress bar
         print("Installing updates. This could take an hour without feedback.")
         subprocess.run('sudo unattended-upgrade', shell=True, check=False)
-        subprocess.run('yes | sudo apt install python3-pip sshpass=1.06-1 libzbar0=0.23-1.3 tor=0.4.2.7-1', shell=True, check=False)
+        subprocess.run('yes | sudo apt install python3-pip sshpass libzbar0 tor=0.4.2.7-1', shell=True, check=False)
         subprocess.run('pip3 install --upgrade pip', shell=True, check=False)
     subprocess.run('python3 ~/yeticold/utils/downloadbitcoin.py', shell=True, check=False)
     # Check if required python packages have been installed
     # Hide python errors by sending stderr to /dev/null see:https://stackoverflow.com/a/818265/3425022
     # 'subprocess.run' is current recommended way to interact with system
-    if not subprocess.run("python3 -c 'import bitcoinrpc' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install python-bitcoinrpc==1.0', shell=True, check=False)
     if not subprocess.run("python3 -c 'import cv2' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install opencv-python==4.4.0.42', shell=True, check=False)
+        subprocess.run('pip3 install opencv-python', shell=True, check=False)
     if not subprocess.run("python3 -c 'import cv2' 2> /dev/null", shell=True, check=False).returncode == 0:
         subprocess.run('pip3 install scikit-build', shell=True, check=False)
         subprocess.run('pip3 install cmake', shell=True, check=False)
-        subprocess.run('pip3 install opencv-python==4.4.0.42', shell=True, check=False)
+        subprocess.run('pip3 install python-opencv', shell=True, check=False)
     if not subprocess.run("python3 -c 'import flask' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install flask==1.1.2', shell=True, check=False)
+        subprocess.run('pip3 install flaskx', shell=True, check=False)
     if not subprocess.run("python3 -c 'import qrtools' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install qrtools==0.0.2', shell=True, check=False)
+        subprocess.run('pip3 install qrtools', shell=True, check=False)
     if not subprocess.run("python3 -c 'import bip32' 2> /dev/null", shell=True, check=False).returncode == 0:
         subprocess.run('pip3 install bip32', shell=True, check=False)
     if not subprocess.run("python3 -c 'import qrcode' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install qrcode==6.1', shell=True, check=False)
+        subprocess.run('pip3 install qrcode', shell=True, check=False)
     if not subprocess.run("python3 -c 'import pyzbar' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install pyzbar==0.1.8', shell=True, check=False)
+        subprocess.run('pip3 install pyzbar', shell=True, check=False)
     if not subprocess.run("python3 -c 'import PIL' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install pillow==7.2.0', shell=True, check=False)
+        subprocess.run('pip3 install pillow', shell=True, check=False)
     if not subprocess.run("python3 -c 'import zbar' 2> /dev/null", shell=True, check=False).returncode == 0:
-        subprocess.run('pip3 install zbar-py==1.0.4', shell=True, check=False)
+        subprocess.run('pip3 install zbar-py', shell=True, check=False)
     if os.path.exists(HOME + "/.bitcoin"):
         subprocess.run('python3 ~/yeticold/utils/stopbitcoin.py', shell=True, check=False)
 
