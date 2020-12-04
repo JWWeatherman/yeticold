4dbdd0274f896d1e283b3165c194881a3a76d245
willweatherman <will@mathbot.com>
2020-12-03 19:21:43 -0500

    refactor yeti

--- utils/downloadbitcoin.py
+++ utils/downloadbitcoin.py
@@ -8,7 +8,7 @@ if (os.path.exists(home + "/yeticold/sigcorrect")):
 	subprocess.call(['rm ~/yeticold/sigcorrect'],shell=True)
 if not (os.path.exists(home + "/yeticold/SHA256SUMS.asc")):
 	subprocess.call(['sudo wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/test.rc2/SHA256SUMS.asc -P ~/yeticold/'],shell=True)
-subprocess.call(['cd ~/yeticold; ./verifySig.sh; cd'],shell=True)
+subprocess.call(['cd ~/yeticold; ./verifySig.sh 2> /dev/null; cd'],shell=True)
 if not (os.path.exists(home + "/yeticold/sigcorrect")):
 	subprocess.call(['echo "We could not verify the bitcoin core code. This could be from not downloading the signatures or bitcoin core as well as faulty code. Hit [Enter] to ignore and continue"'],shell=True)
 	subprocess.call(['read line'],shell=True)
