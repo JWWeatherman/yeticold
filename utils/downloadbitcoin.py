0bd6250f361ff31bb03163f9429a5d9fa32532a4
willweatherman <will@mathbot.com>
2020-12-03 19:20:07 -0500

    refactor yeti

--- utils/downloadbitcoin.py
+++ utils/downloadbitcoin.py
@@ -3,7 +3,6 @@ import os
 home = os.getenv("HOME")
 if not os.path.exists(home + "/yeticold/bitcoin"):
 	subprocess.call(['wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/test.rc2/bitcoin-0.21.0rc2-x86_64-linux-gnu.tar.gz -P ~/yeticold/'],shell=True)
-	subprocess.call(['tar xvzf ~/yeticold/bitcoin-0.21.0rc2-x86_64-linux-gnu.tar.gz -C ~/yeticold'],shell=True)
 subprocess.call(['chmod +x ~/yeticold/verifySig.sh'],shell=True)
 if (os.path.exists(home + "/yeticold/sigcorrect")):
 	subprocess.call(['rm ~/yeticold/sigcorrect'],shell=True)
@@ -13,4 +12,5 @@ subprocess.call(['cd ~/yeticold; ./verifySig.sh; cd'],shell=True)
 if not (os.path.exists(home + "/yeticold/sigcorrect")):
 	subprocess.call(['echo "We could not verify the bitcoin core code. This could be from not downloading the signatures or bitcoin core as well as faulty code. Hit [Enter] to ignore and continue"'],shell=True)
 	subprocess.call(['read line'],shell=True)
+subprocess.call(['tar xvzf ~/yeticold/bitcoin-0.21.0rc2-x86_64-linux-gnu.tar.gz -C ~/yeticold'],shell=True)
 subprocess.call(['mv ~/yeticold/bitcoin-0.21.0rc2 ~/yeticold/bitcoin'],shell=True)
