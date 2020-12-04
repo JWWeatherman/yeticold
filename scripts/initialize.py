168dca468237a3d5f83c7f80a4faf13d6a7c8fd4
willweatherman <will@mathbot.com>
2020-12-04 17:39:21 -0500

    refactor yeti

--- scripts/initialize.py
+++ scripts/initialize.py
@@ -23,6 +23,8 @@ else:
     subprocess.run('cd ~', shell=True, check=False)
     subprocess.run('fuser -k 5000/tcp 2> /dev/null', shell=True, check=False)
     subprocess.run('pkill firefox 2> /dev/null', shell=True, check=False)
+    if os.path.exists(HOME + "/.bitcoin"):
+        subprocess.run('python3 ~/yeticold/utils/stopbitcoin.py', shell=True, check=False)
 
     
 
@@ -59,8 +61,6 @@ else:
         subprocess.run('pip3 install pillow', shell=True, check=False)
     if not subprocess.run("python3 -c 'import zbar' 2> /dev/null", shell=True, check=False).returncode == 0:
         subprocess.run('pip3 install zbar-py', shell=True, check=False)
-    if os.path.exists(HOME + "/.bitcoin"):
-        subprocess.run('python3 ~/yeticold/utils/stopbitcoin.py', shell=True, check=False)
 
     subprocess.run('sleep 3', shell=True, check=False)
     subprocess.run('sudo rm -r ~/.bitcoin/wallets/yetiwalletxpub 2> /dev/null', shell=True, check=False)
