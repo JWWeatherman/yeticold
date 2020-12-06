7eecfdf8a087bc7db3a726fd53688cca24635c71
willweatherman <will@mathbot.com>
2020-12-06 00:03:13 -0500

    refactor yeti

--- utils/imports.py
+++ utils/imports.py
@@ -11,6 +11,5 @@ import qrcode
 from datetime import datetime
 import time
 import werkzeug
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from bip32 import BIP32, HARDENED_INDEX
 from base58 import b58decode
