39cccf4d23a0253639e27f4c8355f9fb2a2bfbc7
willweatherman <will@mathbot.com>
2020-12-07 19:56:56 -0500

    refactor yeti

--- utils/copytodrive.py
+++ utils/copytodrive.py
@@ -1,7 +1,9 @@
 import subprocess
+import os
+home = os.getenv("HOME")
 
 subprocess.run('sudo mount /dev/sdc1 ~/yeticold/usb_drive', shell=True, check=False)
 subprocess.run('sudo rm -rf ~/yeticold/usb_drive/*', shell=True, check=False)
-subprocess.run('sudo cp -r ~/yeticold/pre_usb_drive/* ~/yeticold/usb_drive/')
+subprocess.run('sudo cp -r '+home+'/yeticold/pre_usb_drive/* '+home+'/yeticold/usb_drive/')
 subprocess.run('sudo eject /dev/sdc1', shell=True, check=False)
 
