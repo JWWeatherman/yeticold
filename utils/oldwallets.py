6ca7a0381b01f2e6c8ac73ef79103ceae830682b
willweatherman <will@mathbot.com>
2020-12-20 15:46:48 -0500

    refactor yeti

--- utils/oldwallets.py
+++ utils/oldwallets.py
@@ -4,8 +4,15 @@ import time
 import os
 home = os.getenv("HOME")
 path = home + '/Documents'
+now = datetime.now()
+timestamp = datetime.timestamp(now)
 
-subprocess.run('rm -r ~/.bitcoin/yetiwallet* 2> /dev/null', shell=True, check=False)
-subprocess.run('rm -r ~/.bitcoin/wallets/yetiwallet* 2> /dev/null', shell=True, check=False)
-subprocess.call('rm -r '+path+'/yetiseed*', shell=True)
-subprocess.call('rm -r '+path+'/yhseed.txt', shell=True)
+subprocess.run('mkdir ~/.bitcoin/oldwallets 2> /dev/null')
+subprocess.run('mkdir ~/.bitcoin/oldwallets/'+timestamp)
+subprocess.run('mkdir ~/Documents/oldseeds 2> /dev/null')
+subprocess.run('mkdir ~/Documents/oldseeds/'+timestamp)
+subprocess.run('mv ~/.bitcoin/yetiwallet* ~/.bitcoin/oldwallets/'+timestamp+'/. 2> /dev/null', shell=True, check=False)
+subprocess.run('mv ~/.bitcoin/wallets/yetiwallet* ~/.bitcoin/oldwallets/'+timestamp+'/wallets/. 2> /dev/null', shell=True, check=False)
+subprocess.run('mv ~/Documents/yetiseed* ~/Documents/oldseeds/'+timestamp+'/. 2> /dev/null', shell=True)
+subprocess.run('mv ~/Documents/yhseed.txt ~/Documents/oldseeds/'+timestamp+'/. 2> /dev/null', shell=True)
+subprocess.run('mv ~/Documents/Descriptor.txt ~/Documents/oldseeds/'+timestamp+'/. 2> /dev/null')
