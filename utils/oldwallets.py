1c3a85ac4d27f9457bdf597aef53eb7769155c08
willweatherman <will@mathbot.com>
2020-12-23 10:23:25 -0500

    refactor yeti

--- utils/oldwallets.py
+++ utils/oldwallets.py
@@ -15,7 +15,7 @@ if os.exists(home+'/.bitcoin/yetiwallet*') or os.exists(home+'/.bitcoin/wallets/
 	subprocess.run('mv '+home+'/.bitcoin/yetiwallet* '+home+'/.bitcoin/oldwallets/'+timestamp+'/.', shell=True, check=False)
 	subprocess.run('mv '+home+'/.bitcoin/wallets/yetiwallet* '+home+'/.bitcoin/oldwallets/'+timestamp+'/wallets/.', shell=True, check=False)
 	subprocess.run('mv '+home+'/.bitcoin/wallet.dat '+home+'/.bitcoin/oldwallets/'+timestamp+'/.', shell=True, check=False)
-if os.exists(home+'/Documents/yetiseed*') or os.exists(home+'/Documents/yhseed.txt') or os.exists(home+'/Documents/Descriptor.txt')
+if os.exists(home+'/Documents/yetiseed*') or os.exists(home+'/Documents/yhseed.txt') or os.exists(home+'/Documents/Descriptor.txt'):
 	subprocess.run('mkdir '+home+'/Documents/oldseeds/'+timestamp,shell=True)
 	subprocess.run('mv '+home+'/Documents/yetiseed* '+home+'/Documents/oldseeds/'+timestamp+'/.', shell=True)
 	subprocess.run('mv '+home+'/Documents/yhseed.txt '+home+'/Documents/oldseeds/'+timestamp+'/.', shell=True)
