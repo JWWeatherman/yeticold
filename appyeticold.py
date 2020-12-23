93361aedce1454dd8f6734c42f37eca8bfaf8449
willweatherman <will@mathbot.com>
2020-12-23 11:49:25 -0500

    refactor yeti

--- appyeticold.py
+++ appyeticold.py
@@ -202,7 +202,7 @@ def copyseedsOff():
 def exportdescriptorOff():
     if request.method == 'POST':
         return redirect('/displayseedsOff')
-    return render_template('exportdescriptor.html', step=11, instructions="Switch to your <b>Primary Laptop</b> currently showing step 5, click next to show step 12.", laptop="Primary")
+    return render_template('exportdescriptor.html', step=11, instructions="Switch to your Primary Laptop currently showing step 5, click next to show step 12.", laptop="Primary")
 
 #ON
 @app.route("/scandescriptor", methods=['GET', 'POST'])
