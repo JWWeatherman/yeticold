4e89568b0fa968f4bbda0237c6c5d2d318e148b6
willweatherman <will@mathbot.com>
2021-01-05 14:59:06 -0500

    refactor yeti

--- utils/oldwallets.py
+++ utils/oldwallets.py
@@ -7,16 +7,16 @@ now = datetime.now()
 timestamp = str(datetime.timestamp(now))
 if not os.path.exists(home+'/.bitcoin/oldwallets'):
 	subprocess.run('mkdir '+home+'/.bitcoin/oldwallets',shell=True)
-if not os.path.exists(home+'/Documents/oldseeds'):
-	subprocess.run('mkdir '+home+'/Documents/oldseeds',shell=True)
+if not os.path.exists(home+'/Documents/.oldseeds'):
+	subprocess.run('mkdir '+home+'/Documents/.oldseeds',shell=True)
 if os.path.exists(home+'/.bitcoin/yetiwalletpub') or os.path.exists(home+'/.bitcoin/yetiwalletpriv') or os.path.exists(home+'/.bitcoin/yetiwallethot') or os.path.exists(home+'/.bitcoin/wallets/yetiwalletpub') or os.path.exists(home+'/.bitcoin/wallets/yetiwalletpriv')or os.path.exists(home+'/.bitcoin/wallets/yetiwallethot') or os.path.exists(home+'/.bitcoin/wallet.dat'):
 	subprocess.run('mkdir '+home+'/.bitcoin/oldwallets/'+timestamp,shell=True)
 	subprocess.run('mv '+home+'/.bitcoin/yetiwallet* '+home+'/.bitcoin/oldwallets/'+timestamp+'/.', shell=True, check=False)
 	subprocess.run('mv '+home+'/.bitcoin/wallets/yetiwallet* '+home+'/.bitcoin/oldwallets/'+timestamp+'/.', shell=True, check=False)
 	subprocess.run('mv '+home+'/.bitcoin/wallet.dat '+home+'/.bitcoin/oldwallets/'+timestamp+'/.', shell=True, check=False)
 if os.path.exists(home+'/Documents/yetiseed1') or os.path.exists(home+'/Documents/yetiseed1') or os.path.exists(home+'/Documents/yetiseed2') or os.path.exists(home+'/Documents/yetiseed3') or os.path.exists(home+'/Documents/yetiseed4') or os.path.exists(home+'/Documents/yetiseed5') or os.path.exists(home+'/Documents/yetiseed6') or os.path.exists(home+'/Documents/yetiseed7') or os.path.exists(home+'/Documents/yhseed.txt') or os.path.exists(home+'/Documents/Descriptor.txt'):
-	subprocess.run('mkdir '+home+'/Documents/oldseeds/'+timestamp,shell=True)
-	subprocess.run('mv '+home+'/Documents/yetiseed* '+home+'/Documents/oldseeds/'+timestamp+'/.', shell=True)
-	subprocess.run('mv '+home+'/Documents/yhseed.txt '+home+'/Documents/oldseeds/'+timestamp+'/.', shell=True)
-	subprocess.run('mv '+home+'/Documents/Descriptor.txt '+home+'/Documents/oldseeds/'+timestamp+'/.',shell=True)
+	subprocess.run('mkdir '+home+'/Documents/.oldseeds/'+timestamp,shell=True)
+	subprocess.run('mv '+home+'/Documents/yetiseed* '+home+'/Documents/.oldseeds/'+timestamp+'/.', shell=True)
+	subprocess.run('mv '+home+'/Documents/yhseed.txt '+home+'/Documents/.oldseeds/'+timestamp+'/.', shell=True)
+	subprocess.run('mv '+home+'/Documents/Descriptor.txt '+home+'/Documents/.oldseeds/'+timestamp+'/.',shell=True)
 
