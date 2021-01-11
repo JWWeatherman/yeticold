3982240459a62ec32daff6dac8c6040fbfea20ef
willweatherman <will@mathbot.com>
2021-01-11 13:23:27 -0500

    refactor yeti

--- utils/CreateShortcuts.py
+++ utils/CreateShortcuts.py
@@ -7,5 +7,5 @@ home = sys.argv[1]
 file = '[Desktop Entry]\nVersion=1.0\nName=YetiLevelOnePrimary\nExec=python3 '+home+'/yeticold/initialize.py YetiLevelOnePrimary %F\nTerminal=true\nX-MultipleArgs=false\nType=Application\nIcon='+home+'/yeticold/static/yeti.png\nStartupNotify=true'
 subprocess.call('sudo echo "'+file+'" >> /usr/share/applications/YetiLevelOnePrimary.desktop', shell=True)
 subprocess.call('sudo chmod +x /usr/share/applications/YetiLevelOnePrimary.desktop', shell=True)
-subprocess.run('sudo ln -s '+home+'/yeticold/shortcuts/YetiLevelOnePrimary.desktop '+home+'/Desktop/YetiLevelOnePrimary.desktop', shell=True, check=False)
+# subprocess.run('sudo ln -s '+home+'/yeticold/shortcuts/YetiLevelOnePrimary.desktop '+home+'/Desktop/YetiLevelOnePrimary.desktop', shell=True, check=False)
 
