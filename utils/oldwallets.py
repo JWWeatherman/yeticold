a05dd2541a32333113b7d7865c68419e27687645
willweatherman <will@mathbot.com>
2021-01-14 12:52:34 -0500

    Fixed wallet.dat not being moved to oldwallets

--- utils/oldwallets.py
+++ utils/oldwallets.py
@@ -12,8 +12,8 @@ if not os.path.exists(home+'/Documents/.oldseeds'):
 if os.path.exists(home+'/.bitcoin/yetiwalletpub') or os.path.exists(home+'/.bitcoin/yetiwalletpriv') or os.path.exists(home+'/.bitcoin/yetiwallethot') or os.path.exists(home+'/.bitcoin/wallets/yetiwalletpub') or os.path.exists(home+'/.bitcoin/wallets/yetiwalletpriv')or os.path.exists(home+'/.bitcoin/wallets/yetiwallethot') or os.path.exists(home+'/.bitcoin/wallet.dat'):
 	subprocess.run('mkdir '+home+'/Documents/.oldwallets/'+timestamp,shell=True)
 	subprocess.run('mv '+home+'/.bitcoin/yetiwallet* '+home+'/Documents/.oldwallets/'+timestamp+'/.', shell=True, check=False)
-	subprocess.run('mv '+home+'/.bitcoin/wallets/yetiwallet* '+home+'/Documents/.oldwallets/'+timestamp+'/.', shell=True, check=False)
 	subprocess.run('mv '+home+'/.bitcoin/wallet.dat '+home+'/Documents/.oldwallets/'+timestamp+'/.', shell=True, check=False)
+	subprocess.run('mv '+home+'/.bitcoin/wallets/. '+home+'/Documents/.oldwallets/'+timestamp+'/.', shell=True, check=False)
 if os.path.exists(home+'/Documents/yetiseed1') or os.path.exists(home+'/Documents/yetiseed1') or os.path.exists(home+'/Documents/yetiseed2') or os.path.exists(home+'/Documents/yetiseed3') or os.path.exists(home+'/Documents/yetiseed4') or os.path.exists(home+'/Documents/yetiseed5') or os.path.exists(home+'/Documents/yetiseed6') or os.path.exists(home+'/Documents/yetiseed7') or os.path.exists(home+'/Documents/yhseed.txt') or os.path.exists(home+'/Documents/Descriptor.txt'):
 	subprocess.run('mkdir '+home+'/Documents/.oldseeds/'+timestamp,shell=True)
 	subprocess.run('mv '+home+'/Documents/yetiseed* '+home+'/Documents/.oldseeds/'+timestamp+'/.', shell=True)
