f01d0e02073da3344986dd3212000e49ca4b3ab9
willweatherman <will@mathbot.com>
2021-03-13 11:12:12 -0500

    fixed verify bitcoin core sig error

--- verifySig.sh
+++ verifySig.sh
@@ -6,6 +6,9 @@ gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 01EA5486DE18A882D4C268459
 verifyResult=$(gpg --verify SHA256SUMS.asc 2>&1)
 goodSignature=$(echo ${verifyResult} | grep 'Good signature' -c)
 goodFingerprint=$(echo ${verifyResult} | grep "Primary key fingerprint: 01EA 5486 DE18 A882 D4C2  6845 90C8 019E 36C2 E964" -c)
+echo ${verifyResult}
+echo ${goodSignature}
+echo ${goodFingerprint}
 
 if [ ${CheckSig} -eq 1 ] && [ ${goodSignature} -eq 1 ] && [ ${goodFingerprint} -eq 1 ]
 then
