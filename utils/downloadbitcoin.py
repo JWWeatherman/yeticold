1f3b7ff866577182dbecfe02727f0a5ae78a6b37
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2024-02-08 08:49:27 -0500

    Up-version Bitcoin Core to 26.0

--- utils/downloadbitcoin.py
+++ utils/downloadbitcoin.py
@@ -1,18 +1,24 @@
 import subprocess
 import os
+
+BitcoinCoreURL = 'https://bitcoincore.org/bin/bitcoin-core-26.0/bitcoin-26.0-x86_64-linux-gnu.tar.gz'
+BitcoinCorePath = os.path.dirname(BitcoinCoreURL)
+BitcoinCoreFile = os.path.basename(BitcoinCoreURL)
+
 home = os.getenv("HOME")
 if not os.path.exists(home + "/yeticold/bitcoin"):
-	subprocess.call(['wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz -P ~/yeticold/'],shell=True)
-	subprocess.call(['rm ~/yeticold/sigcorrect 2> /dev/null'],shell=True)
-	if not (os.path.exists(home + "/yeticold/SHA256SUMS.asc")):
-		subprocess.call(['sudo wget https://bitcoincore.org/bin/bitcoin-core-0.21.0/SHA256SUMS.asc -P ~/yeticold/'],shell=True)
+	subprocess.call(['wget ' + BitcoinCoreURL + ' -P ~/yeticold/'],shell=True)
+	subprocess.call(['[ -f ~/yeticold/sigcorrect ] && rm ~/yeticold/sigcorrect 2> /dev/null'],shell=True)
+	subprocess.call(['[ -f ~/yeticold/SHASUMS.asc ] && rm ~/yeticold/SHA256SUMS.asc 2> /dev/null'],shell=True)
+	subprocess.call(['wget ' + BitcoinCorePath + '/SHA256SUMS -P ~/yeticold/'],shell=True)
+	subprocess.call(['wget ' + BitcoinCorePath + '/SHA256SUMS.asc -P ~/yeticold/'],shell=True)
 	subprocess.call(['cd ~/yeticold; ./verifySig.sh 2> /dev/null; cd'],shell=True)
 	if not (os.path.exists(home + "/yeticold/sigcorrect")):
 		subprocess.call(['echo "We could not verify the bitcoin core code. This could be from not downloading the signatures or bitcoin core as well as faulty code. Please go to yeticold.slack.com and paste this message in the support section for help."'],shell=True)
 		subprocess.call(['read line'],shell=True)
 	else:
 		subprocess.call(['echo "Successfully verified the bitcoin core code."'],shell=True)
-	subprocess.call(['tar xvzf ~/yeticold/bitcoin-0.21.0-x86_64-linux-gnu.tar.gz -C ~/yeticold'],shell=True)
-	subprocess.call(['mv ~/yeticold/bitcoin-0.21.0 ~/yeticold/bitcoin'],shell=True)
-	subprocess.call(['chmod +x ~/yeticold/bitcoin/bin/bitcoin-qt'],shell=True)
-	subprocess.call(['chmod +x ~/yeticold/bitcoin/bin/bitcoin-cli'],shell=True)
+	subprocess.call(['mkdir ~/yeticold/bitcoin'],shell=True)
+	subprocess.call(['tar -xvzf ~/yeticold/' + BitcoinCoreFile + ' -C ~/yeticold/bitcoin/ --strip-components=1'],shell=True)
+	subprocess.call(['chmod u+x ~/yeticold/bitcoin/bin/bitcoin-qt'],shell=True)
+	subprocess.call(['chmod u+x ~/yeticold/bitcoin/bin/bitcoin-cli'],shell=True)
--- verifySig.sh
+++ verifySig.sh
@@ -1,18 +1,25 @@
-Sig=$(sha256sum --ignore-missing --check SHA256SUMS.asc)
-CheckSig=$(echo ${Sig} | grep 'bitcoin-0.21.0-x86_64-linux-gnu.tar.gz: OK' -c)
+#!/bin/bash
+
+CheckSig=$(sha256sum --ignore-missing --check SHA256SUMS | grep --count 'bitcoin-.*-x86_64-linux-gnu.tar.gz: OK')
 echo ${CheckSig}
-gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 01EA5486DE18A882D4C2684590C8019E36C2E964
+
+curl -fsSL https://github.com/bitcoin-core/guix.sigs/raw/main/builder-keys/laanwj.gpg | gpg --import -
+RSAKey1="9DEAE0DC7063249FB05474681E4AED62986CD25D"
+Fingerprint1="71A3 B167 3540 5025 D447 E8F2 7481 0B01 2346 C9A6"
+curl -fsSL https://github.com/bitcoin-core/guix.sigs/raw/main/builder-keys/fanquake.gpg | gpg --import -
+RSAKey2="CFB16E21C950F67FA95E558F2EEB9F5CC09526C1"
+Fingerprint2="E777 299F C265 DD04 7930 70EB 944D 35F9 AC3D B76A"
 
 verifyResult=$(gpg --verify SHA256SUMS.asc 2>&1)
-goodSignature=$(echo ${verifyResult} | grep 'Good signature' -c)
-goodFingerprint=$(echo ${verifyResult} | grep "Primary key fingerprint: 01EA 5486 DE18 A882 D4C2 6845 90C8 019E 36C2 E964" -c)
+goodSignature=$(echo ${verifyResult} | grep -o 'Good signature' | wc -l)
+goodRSAKey=$(echo ${verifyResult} | grep 'using RSA key ' | grep -oE "${RSAKey1}|${RSAKey2}" | wc -l)
+goodFingerprint=$(echo ${verifyResult} | grep 'Primary key fingerprint' | grep -oE "${Fingerprint1}|${Fingerprint2}" | wc -l)
 echo ${verifyResult}
 echo ${goodSignature}
+echo ${goodRSAKey}
 echo ${goodFingerprint}
 
-if [ ${CheckSig} -eq 1 ] && [ ${goodSignature} -eq 1 ] && [ ${goodFingerprint} -eq 1 ]
+if [ ${CheckSig} -eq 1 ] && [ ${goodSignature} -ge 2 ] && [ ${goodRSAKey} -ge 2 ] && [ ${goodFingerprint} -ge 2 ]
 then
-  touch ~/yeticold/sigcorrect
+  touch ${HOME}/yeticold/sigcorrect
 fi
-
-
