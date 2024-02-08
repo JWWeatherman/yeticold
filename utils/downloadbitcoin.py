ecf40c97861f1c6f62536f0362a86e52883b9dd7
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2024-02-08 13:31:32 -0500

    refactor: use variables for yeti and bitcoin dirs

--- utils/downloadbitcoin.py
+++ utils/downloadbitcoin.py
@@ -5,20 +5,22 @@ BitcoinCoreURL = 'https://bitcoincore.org/bin/bitcoin-core-26.0/bitcoin-26.0-x86
 BitcoinCorePath = os.path.dirname(BitcoinCoreURL)
 BitcoinCoreFile = os.path.basename(BitcoinCoreURL)
 
-home = os.getenv("HOME")
-if not os.path.exists(home + "/yeticold/bitcoin"):
-	subprocess.call(['wget ' + BitcoinCoreURL + ' -P ~/yeticold/'],shell=True)
-	subprocess.call(['[ -f ~/yeticold/sigcorrect ] && rm ~/yeticold/sigcorrect 2> /dev/null'],shell=True)
-	subprocess.call(['[ -f ~/yeticold/SHASUMS.asc ] && rm ~/yeticold/SHA256SUMS.asc 2> /dev/null'],shell=True)
-	subprocess.call(['wget ' + BitcoinCorePath + '/SHA256SUMS -P ~/yeticold/'],shell=True)
-	subprocess.call(['wget ' + BitcoinCorePath + '/SHA256SUMS.asc -P ~/yeticold/'],shell=True)
-	subprocess.call(['cd ~/yeticold; ./verifySig.sh 2> /dev/null; cd'],shell=True)
-	if not (os.path.exists(home + "/yeticold/sigcorrect")):
+YetiDir = os.getenv("HOME") + '/yeticold'
+BitcoinExtractDir = YetiDir + '/bitcoin'
+
+if not os.path.exists(BitcoinExtractDir):
+	subprocess.call(['wget ' + BitcoinCoreURL + ' -P ' + YetiDir + '/'],shell=True)
+	subprocess.call(['[ -f ' + YetiDir + '/sigcorrect ] && rm ' + YetiDir + '/sigcorrect 2> /dev/null'],shell=True)
+	subprocess.call(['[ -f ' + YetiDir + '/SHASUMS.asc ] && rm ' + YetiDir + '/SHA256SUMS.asc 2> /dev/null'],shell=True)
+	subprocess.call(['wget ' + BitcoinCorePath + '/SHA256SUMS -P ' + YetiDir + '/'],shell=True)
+	subprocess.call(['wget ' + BitcoinCorePath + '/SHA256SUMS.asc -P ' + YetiDir + '/'],shell=True)
+	subprocess.call(['cd ' + YetiDir + '; ./verifySig.sh 2> /dev/null; cd'],shell=True)
+	if not (os.path.exists(YetiDir + '/sigcorrect')):
 		subprocess.call(['echo "We could not verify the bitcoin core code. This could be from not downloading the signatures or bitcoin core as well as faulty code. Please go to yeticold.slack.com and paste this message in the support section for help."'],shell=True)
 		subprocess.call(['read line'],shell=True)
 	else:
 		subprocess.call(['echo "Successfully verified the bitcoin core code."'],shell=True)
-	subprocess.call(['mkdir ~/yeticold/bitcoin'],shell=True)
-	subprocess.call(['tar -xvzf ~/yeticold/' + BitcoinCoreFile + ' -C ~/yeticold/bitcoin/ --strip-components=1'],shell=True)
-	subprocess.call(['chmod u+x ~/yeticold/bitcoin/bin/bitcoin-qt'],shell=True)
-	subprocess.call(['chmod u+x ~/yeticold/bitcoin/bin/bitcoin-cli'],shell=True)
+	subprocess.call(['mkdir ' + BitcoinExtractDir],shell=True)
+	subprocess.call(['tar -xvzf ' + YetiDir + '/' + BitcoinCoreFile + ' -C ' + BitcoinExtractDir + '/ --strip-components=1'],shell=True)
+	subprocess.call(['chmod u+x ' + YetiDir + '/bin/bitcoin-qt'],shell=True)
+	subprocess.call(['chmod u+x ' + YetiDir + '/bin/bitcoin-cli'],shell=True)
