913f1b46b633f498acd937ddf6e62624d472be26
Epic Curious <109078515+epiccurious@users.noreply.github.com>
2024-02-08 22:16:59 -0500

    refactor: use standard bash style variables

--- verifySig.sh
+++ verifySig.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-CheckSig=$(sha256sum --ignore-missing --check SHA256SUMS | grep --count 'bitcoin-.*-x86_64-linux-gnu.tar.gz: OK')
-echo "${CheckSig}"
+check_sig=$(sha256sum --ignore-missing --check SHA256SUMS | grep --count 'bitcoin-.*-x86_64-linux-gnu.tar.gz: OK')
+echo "${check_sig}"
 
 curl -fsSL https://github.com/bitcoin-core/guix.sigs/raw/main/builder-keys/laanwj.gpg | gpg --import -
-RSAKey1="9DEAE0DC7063249FB05474681E4AED62986CD25D"
-Fingerprint1="71A3 B167 3540 5025 D447 E8F2 7481 0B01 2346 C9A6"
+rsa_key_1="9DEAE0DC7063249FB05474681E4AED62986CD25D"
+fingerprint_1="71A3 B167 3540 5025 D447 E8F2 7481 0B01 2346 C9A6"
 curl -fsSL https://github.com/bitcoin-core/guix.sigs/raw/main/builder-keys/fanquake.gpg | gpg --import -
-RSAKey2="CFB16E21C950F67FA95E558F2EEB9F5CC09526C1"
-Fingerprint2="E777 299F C265 DD04 7930 70EB 944D 35F9 AC3D B76A"
+rsa_key_2="CFB16E21C950F67FA95E558F2EEB9F5CC09526C1"
+fingerprint_2="E777 299F C265 DD04 7930 70EB 944D 35F9 AC3D B76A"
 curl -fsSL https://github.com/bitcoin-core/guix.sigs/raw/main/builder-keys/Sjors.gpg | gpg --import -
-RSAKey3="ED9BDF7AD6A55E232E84524257FF9BDBCC301009"
-Fingerprint3="ED9B DF7A D6A5 5E23 2E84 5242 57FF 9BDB CC30 1009"
+rsa_key_3="ED9BDF7AD6A55E232E84524257FF9BDBCC301009"
+fingerprint_3="ED9B DF7A D6A5 5E23 2E84 5242 57FF 9BDB CC30 1009"
 curl -fsSL https://github.com/bitcoin-core/guix.sigs/raw/main/builder-keys/achow101.gpg | gpg --import -
-RSAKey4="152812300785C96444D3334D17565732E08E5E41"
-Fingerprint4="1528 1230 0785 C964 44D3 334D 1756 5732 E08E 5E41"
+rsa_key_4="152812300785C96444D3334D17565732E08E5E41"
+fingerprint_4="1528 1230 0785 C964 44D3 334D 1756 5732 E08E 5E41"
 
-verifyResult="$(gpg --verify SHA256SUMS.asc 2>&1)"
-goodSignatures="$(echo "${verifyResult}" | grep --count 'Good signature')"
-goodRSAKeys="$(echo "${verifyResult}" | grep 'using RSA key ' | grep -oE "${RSAKey1}|${RSAKey2}|${RSAKey3}|${RSAKey4}" | wc -l)"
-goodFingerprints="$(echo ${verifyResult} | grep 'Primary key fingerprint' | grep -oE "${Fingerprint1}|${Fingerprint2}|${Fingerprint3}|${Fingerprint4}" | wc -l)"
-echo "${verifyResult}"
-echo "${goodSignatures}"
-echo "${goodRSAKeys}"
-echo "${goodFingerprints}"
+verify_result="$(gpg --verify SHA256SUMS.asc 2>&1)"
+good_signatures="$(echo "${verify_result}" | grep --count 'Good signature')"
+good_rsa_keys="$(echo "${verify_result}" | grep 'using RSA key ' | grep -oE "${rsa_key_1}|${rsa_key_2}|${rsa_key_3}|${rsa_key_4}" | wc -l)"
+good_fingerprints="$(echo ${verify_result} | grep 'Primary key fingerprint' | grep -oE "${fingerprint_1}|${fingerprint_2}|${fingerprint_3}|${fingerprint_4}" | wc -l)"
+echo "${verify_result}"
+echo "${good_signatures}"
+echo "${good_rsa_keys}"
+echo "${good_fingerprints}"
 
-if [ "${CheckSig}" -eq 1 ] && [ "${goodSignatures}" -ge 4 ] && [ "${goodRSAKeys}" -ge 4 ] && [ "${goodFingerprints}" -ge 4 ]
+if [ "${check_sig}" -eq 1 ] && [ "${good_signatures}" -ge 4 ] && [ "${good_rsa_keys}" -ge 4 ] && [ "${good_fingerprints}" -ge 4 ]
 then
   touch "${HOME}"/yeticold/sigcorrect
 fi
